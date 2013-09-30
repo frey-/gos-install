d6dc82520dc285750626b39f38434c2f22320db1
Bob131 <aeroflyable@gmail.com>
2013-10-01 04:41:43 +1000

    Removed more deps; Added a message prefix variable

--- build.sh
+++ build.sh
@@ -11,41 +11,43 @@ STAGE3_DATE="20130822"
 ARCH="amd64"
 FILELOC=`pwd`
 
+MSGPREFIX=" !!!"
+
 
 if ["$1" == '']; then
     echo "Specify disk"
     exit 0;
 fi
 
-echo "!!! Creating filesystem"
+echo "$MSGPREFIX Creating filesystem"
 mkfs.ext3 -q /dev/$DISK
 
-echo "!!! Mounting filesystem"
+echo "$MSGPREFIX Mounting filesystem"
 mount /dev/$DISK /mnt/gentoo
 cd /mnt/gentoo
 
-echo "!!! Fetching stage3"
+echo "$MSGPREFIX Fetching stage3"
 wget http://distfiles.gentoo.org/releases/${ARCH}/current-stage3/stage3-${ARCH}-${STAGE3_DATE}.tar.bz2
 
-echo "!!! Unpacking stage3"
+echo "$MSGPREFIX Unpacking stage3"
 tar -xf stage3*
 
-echo "!!! Mounting system directories"
+echo "$MSGPREFIX Mounting system directories"
 cd /
 mount -t proc proc /mnt/gentoo/proc
 mount --rbind /dev /mnt/gentoo/dev
 mount --rbind /sys /mnt/gentoo/sys
 cp -L /etc/resolv.conf /mnt/gentoo/etc/
 
-echo "!!! Installing stage 2"
+echo "$MSGPREFIX Installing stage 2"
 printf "DISK=${DISK}\n\n" > /mnt/gentoo/build_stage2.sh
 cat ${FILELOC}/build_stage2.sh >> /mnt/gentoo/build_stage2.sh
 chmod +x /mnt/gentoo/build_stage2.sh
 
-echo "!!! Creating another make.conf and patching"
+echo "$MSGPREFIX Creating another make.conf and patching"
 cp /mnt/gentoo/etc/portage/make.conf /mnt/gentoo/etc/portage/make2.conf
 cat $FILELOC/make >> /mnt/gentoo/etc/portage/make2.conf
 
-echo "!!! Chrooting"
-echo "!!! Stage 1 complete. Please run build_stage2.sh to continue"
+echo "$MSGPREFIX Chrooting"
+echo "$MSGPREFIX Stage 1 complete. Please run build_stage2.sh to continue"
 chroot /mnt/gentoo /bin/bash
--- build_stage2.sh
+++ build_stage2.sh
@@ -2,54 +2,58 @@ PACKAGES="syslog-ng vixie-cron firefox gvim emacs mpv rxvt-unicode tmux weston w
 PACKAGES="${PACKAGES} =kde-base/kdeadmin-meta-4.11.1 =kde-base/kdebase-meta-4.11.1 =kde-base/kdebase-runtime-meta-4.11.1 =kde-base/kdemultimedia-meta-4.11.1 =kde-base/kdenetwork-meta-4.11.1 =kde-base/kdeutils-meta-4.11.1"
 PACKAGES="${PACKAGES} =x11-base/xorg-server-9999-r1 oh-my-zsh @qt5-essentials @qt5-addons"
 
+MSGPREFIX=" !!!"
 
-echo "!!! Evaluating profile"
+
+echo "$MSGPREFIX Evaluating profile"
 source /etc/profile
 
-echo "!!! Fetching portage tree"
+echo "$MSGPREFIX Fetching portage tree"
 emerge-webrsync
 
-echo "!!! Setting profile"
+echo "$MSGPREFIX Setting profile"
 eselect profile set default/linux/amd64/13.0/desktop/kde
 
-echo "!!! Emerging kernel and generation tools"
+echo "$MSGPREFIX Emerging kernel and generation tools"
 emerge --quiet --quiet-build gentoo-sources genkernel
 
-echo "!!! Building kernel"
+echo "$MSGPREFIX Building kernel"
 genkernel --no-menuconfig kernel
 
-echo "!!! Installing fstab"
+echo "$MSGPREFIX Installing fstab"
 echo "/dev/${DISK}  /  ext3  noatime  0 0" > /etc/fstab
 
-echo "!!! Emerging layman and flaggie"
+echo "$MSGPREFIX Emerging layman and flaggie"
 emerge --quiet --quiet-build flaggie
 flaggie layman +cvs +subversion
-# to remove the massive dependency list that gnupg pulls in
-flaggie git -gpg
+# to remove the massive dependency lists
+flaggie dev-vcs/git -gpg -gtk
+flaggie subversion -kde
+###
 emerge --quiet --quiet-build layman
 
-echo "!!! Installing secondary make.conf"
+echo "$MSGPREFIX Installing secondary make.conf"
 mv /etc/portage/make2.conf /etc/portage/make.conf
 
-echo "!!! Adding overlays"
+echo "$MSGPREFIX Adding overlays"
 layman -o http://aoliynik-overlay.googlecode.com/files/aoliynik-overlay.xml -f -a aoliynik
 layman -o https://raw.github.com/SlashGeeSlashOS/gos-overlay/master/gos-overlay.xml -f -a gos-overlay
 layman -a qt
 
-echo "!!! Adding flags"
+echo "$MSGPREFIX Adding flags"
 flaggie weston +X +fbdev +opengl +wayland-compositor +xwayland
 flaggie wicd +X +gtk +ncurses +libnotify
 
-echo "!!! Emerging packages"
+echo "$MSGPREFIX Emerging packages"
 emerge -v $PACKAGES
 
-echo "!!! Setting up RC"
+echo "$MSGPREFIX Setting up RC"
 rc-update add sshd default
 rc-update add vixie-cron default
 rc-update add syslog-ng default
 rc-update add wicd default
 
-echo "!!! Setting root password\n!!! Please enter new password"
+echo "$MSGPREFIX Setting root password\n!!! Please enter new password"
 passwd
 
-echo "!!! Build script completed. Please set timezone, add new users and configure bootloader before rebooting"
+echo "$MSGPREFIX Build script completed. Please set timezone, add new users and configure bootloader before rebooting"
