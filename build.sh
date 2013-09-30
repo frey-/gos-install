a11a1d10682af7a098ea5e4ce25aa62d7df2eeff
Bob131 <aeroflyable@gmail.com>
2013-10-01 03:07:59 +1000

    Uses emerge-webrsync instead of wget and tar. Messages prefixed with '!!!' to try and help users distinguish script output from other output

--- build.sh
+++ build.sh
@@ -17,35 +17,35 @@ if ["$1" == '']; then
     exit 0;
 fi
 
-echo "Creating filesystem"
+echo "!!! Creating filesystem"
 mkfs.ext3 -q /dev/$DISK
 
-echo "Mounting filesystem"
+echo "!!! Mounting filesystem"
 mount /dev/$DISK /mnt/gentoo
 cd /mnt/gentoo
 
-echo "Fetching stage3"
+echo "!!! Fetching stage3"
 wget http://distfiles.gentoo.org/releases/${ARCH}/current-stage3/stage3-${ARCH}-${STAGE3_DATE}.tar.bz2
 
-echo "Unpacking stage3"
+echo "!!! Unpacking stage3"
 tar -xf stage3*
 
-echo "Mounting system directories"
+echo "!!! Mounting system directories"
 cd /
 mount -t proc proc /mnt/gentoo/proc
 mount --rbind /dev /mnt/gentoo/dev
 mount --rbind /sys /mnt/gentoo/sys
 cp -L /etc/resolv.conf /mnt/gentoo/etc/
 
-echo "Installing stage 2"
+echo "!!! Installing stage 2"
 printf "DISK=${DISK}\n\n" > /mnt/gentoo/build_stage2.sh
 cat ${FILELOC}/build_stage2.sh >> /mnt/gentoo/build_stage2.sh
 chmod +x /mnt/gentoo/build_stage2.sh
 
-echo "Creating another make.conf and patching"
+echo "!!! Creating another make.conf and patching"
 cp /mnt/gentoo/etc/portage/make.conf /mnt/gentoo/etc/portage/make2.conf
 cat $FILELOC/make >> /mnt/gentoo/etc/portage/make2.conf
 
-echo "Chrooting"
-echo "Stage 1 complete. Please run build_stage2.sh to continue"
+echo "!!! Chrooting"
+echo "!!! Stage 1 complete. Please run build_stage2.sh to continue"
 chroot /mnt/gentoo /bin/bash
--- build_stage2.sh
+++ build_stage2.sh
@@ -3,55 +3,51 @@ PACKAGES="${PACKAGES} =kde-base/kdeadmin-meta-4.11.1 =kde-base/kdebase-meta-4.11
 PACKAGES="${PACKAGES} =x11-base/xorg-server-9999-r1 oh-my-zsh"
 
 
-echo "Evaluating profile"
+echo "!!! Evaluating profile"
 source /etc/profile
 
-echo "Fetching portage tree"
-cd /usr/
-wget http://distfiles.gentoo.org/snapshots/portage-latest.tar.bz2
+echo "!!! Fetching portage tree"
+emerge-webrsync
 
-echo "Unpacking portage tree"
-tar -xf portage-latest.tar.bz2
-
-echo "Setting profile"
+echo "!!! Setting profile"
 eselect profile set default/linux/amd64/13.0/desktop/kde
 
-echo "Emerging kernel and generation tools"
+echo "!!! Emerging kernel and generation tools"
 emerge --quiet --quiet-build gentoo-sources genkernel
 
-echo "Building kernel"
+echo "!!! Building kernel"
 genkernel --no-install --no-menuconfig bzImage
 
-echo "Installing kernel"
+echo "!!! Installing kernel"
 cp /usr/src/linux/arch/x86/bzImage /boot/kernel
 
-echo "Installing fstab"
+echo "!!! Installing fstab"
 echo "/dev/${DISK}  /  ext3  noatime  0 0" > /etc/fstab
 
-echo "Emerging layman and flaggie"
+echo "!!! Emerging layman and flaggie"
 emerge --quiet --quiet-build layman flaggie
 
-echo "Installing secondary make.conf"
+echo "!!! Installing secondary make.conf"
 mv /etc/portage/make2.conf /etc/portage/make.conf
 
-echo "Adding aoliynik and /g/OS overlays"
+echo "!!! Adding aoliynik and /g/OS overlays"
 layman -o http://aoliynik-overlay.googlecode.com/files/aoliynik-overlay.xml -f -a aoliynik
 layman -o https://raw.github.com/SlashGeeSlashOS/gos-overlay/master/gos-overlay.xml -f -a gos-overlay
 
-echo "Adding flags"
+echo "!!! Adding flags"
 flaggie weston +X +fbdev +opengl +wayland-compositor +xwayland
 flaggie wicd +X +gtk +ncurses +libnotify
 
-echo "Emerging packages"
+echo "!!! Emerging packages"
 emerge -v $PACKAGES
 
-echo "Setting up RC"
+echo "!!! Setting up RC"
 rc-update add sshd default
 rc-update add vixie-cron default
 rc-update add syslog-ng default
 rc-update add wicd default
 
-echo "Setting root password\nPlease enter new password"
+echo "!!! Setting root password\n!!! Please enter new password"
 passwd
 
-echo "Build script completed. Please set timezone, add new users and configure bootloader before rebooting"
+echo "!!! Build script completed. Please set timezone, add new users and configure bootloader before rebooting"
